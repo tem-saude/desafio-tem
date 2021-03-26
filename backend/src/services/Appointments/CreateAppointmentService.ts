3446a8c7da32fd49345eb57f37ab407c8a6be7a7
Luke0195 <lucas201273@gmail.com>
2021-03-26 20:40:58 -0300

    refactor: simplify validator of requiredFields

--- backend/src/services/Appointments/CreateAppointmentService.ts
+++ backend/src/services/Appointments/CreateAppointmentService.ts
@@ -1,6 +1,7 @@
 import {getCustomRepository} from 'typeorm';
 import AppointmentRepository from '../../repositories/AppoinmentRepository'
 import  Appointment from '../../models/Appointment';
+import { format } from 'node:path';
 
 
  interface Request{
@@ -10,13 +11,21 @@ import  Appointment from '../../models/Appointment';
  }
 export class CreateAppointmentService{
 
-  public async execute({patient, doctor, appointment_date}:Request):Promise<Appointment>{
+  public async execute(data:Request):Promise<Appointment>{
     const appointmentRepository = getCustomRepository(AppointmentRepository);
+    const requiredFields = ['patient', 'doctor', 'appointment_date']
+
+    for(const field of requiredFields){
+      if(!data[field]){
+        throw new Error(`${field} is not provided`)
+      }
+    }
+
 
     const appointment = appointmentRepository.create({
-      patient,
-      doctor,
-      appointment_date
+      patient:data.patient,
+      doctor:data.doctor,
+      appointment_date:data.appointment_date
     })
 
     await appointmentRepository.save(appointment)
