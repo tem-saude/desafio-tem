b54c3b99f269002b0da2b199d843ed5659c07c8f
Luke0195 <lucas201273@gmail.com>
2021-03-26 23:34:24 -0300

    feat: add welcome page

--- frontend/src/pages/Welcome/styles.ts
+++ frontend/src/pages/Welcome/styles.ts
@@ -1,9 +1,31 @@
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
+import {shade} from 'polished'
 import * as colors from '../../config/colors'
+
+
+const showContent = keyframes`
+
+ 0%{
+   opacity: 0;
+ }
+
+ 25%{
+   opacity: 0.5;
+ }
+
+ 50%{
+   opacity: 0 .7
+ }
+
+ 100%{
+   opacity:1;
+ }
+`
 
 export const Container = styled.div`
  width: 100%;
  max-width:600px;
+ height: 300px;
  margin:200px auto;
  padding: 20px;
  border-radius:20px;
@@ -12,6 +34,9 @@ export const Container = styled.div`
  align-items:center;
  justify-content:center;
  text-align:center;
+ box-shadow: 0 0 4px 4px ${colors.textHighlight};
+ background:rgba(255,255,255,0.5);
+ animation: ${showContent} 4s ease-out;
 
  h1{
    font-size: 30px;
@@ -40,10 +65,16 @@ export const Container = styled.div`
    justify-content:center;
    align-items:center;
    border-radius:30px;
+   transition: all 0.8s ease-out;
+
+   &:hover{
+     background:${shade(0.2, '#4cd62b')}
+   }
 
  }
 
 `
+
 
 
 export const Title = styled.strong`
@@ -51,6 +82,7 @@ export const Title = styled.strong`
  cursor: pointer;
 
  &:hover{
+   color:${shade(0.2, '#e83f5b')};
   border-bottom: 1px solid ${colors.red}
  }
 `
