d8240726d0f85b8b174c2ea1f779bbfe0819388b
Luke0195 <lucas201273@gmail.com>
2021-03-30 15:35:38 -0300

    feat: done

--- backend/src/__tests__/Appointment.test.ts
+++ backend/src/__tests__/Appointment.test.ts
@@ -7,32 +7,32 @@ import request from 'supertest'
 describe('Appointments Services', () =>{
 
   test('Should returns 400 if  patient name is not provided', async() =>{
-    const httpResponse ={
+    const httpRequest ={
       doctor: 'Ana',
       appointment_date: '2021-03-30 14:38:53'
     }
 
     const response = await request(app).post('/appointments')
-    .send(httpResponse).expect(400);
+    .send(httpRequest).expect(400);
   })
 
   test('Should returns 400 if  doctor is not provided', async() =>{
-    const httpResponse ={
+    const httpRequest ={
       patient: 'Lucas',
       appointment_date: '2021-03-30 14:38:53'
     }
 
     const response = await request(app).post('/appointments')
-    .send(httpResponse).expect(400);
+    .send(httpRequest).expect(400);
   })
 
-  test('Should returns 400 if appointment_date is  not provided', async() =>{
-    const httpResponse ={
+  test('Should returns 400 if appointment_date is  notprovided', async() =>{
+    const httpRequest ={
       patient: 'Lucas',
       doctor: 'Ana'
     }
     const response = await request(app).post('/appointments')
-    .send(httpResponse).expect(400);
+    .send(httpRequest).expect(400);
   })
 
-  })
+})
