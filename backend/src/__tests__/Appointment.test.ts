f68731080f56e1129b8aab7b88e74824814d8ce4
Luke0195 <lucas201273@gmail.com>
2021-03-30 15:20:29 -0300

    fix: change commit messages

--- backend/src/__tests__/Appointment.test.ts
+++ backend/src/__tests__/Appointment.test.ts
@@ -6,7 +6,7 @@ import request from 'supertest'
 
 describe('Appointments Services', () =>{
 
-  test('Should returns 400 if no patient name is provided', async() =>{
+  test('Should returns 400 if  patient name is not provided', async() =>{
     const httpResponse ={
       doctor: 'Ana',
       appointment_date: '2021-03-30 14:38:53'
@@ -16,12 +16,21 @@ describe('Appointments Services', () =>{
     .send(httpResponse).expect(400);
   })
 
-  test('Should returns 400 if no doctor is provided', async() =>{
+  test('Should returns 400 if  doctor is not provided', async() =>{
     const httpResponse ={
       patient: 'Lucas',
       appointment_date: '2021-03-30 14:38:53'
     }
 
+    const response = await request(app).post('/appointments')
+    .send(httpResponse).expect(400);
+  })
+
+  test('Should returns 400 if appointment_date is  not provided', async() =>{
+    const httpResponse ={
+      patient: 'Lucas',
+      doctor: 'Ana'
+    }
     const response = await request(app).post('/appointments')
     .send(httpResponse).expect(400);
   })
